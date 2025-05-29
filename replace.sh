7044812d7e1aac5f3f5416dff0f39ac93f58eab1
Arnav G. <73305591+arnavg115@users.noreply.github.com>
2025-05-29 14:19:55 -0700

    Update replace.sh

--- replace.sh
+++ replace.sh
@@ -16,6 +16,7 @@ sed -i "s/joints = .*/joints = $new_value/" Snake/planar.py
 pip install -r requirements.txt
 pip install -e .
 
+cd tdmpc2
 
 python train.py model_size=5 steps=1_000_000 buffer_size=500_000 exp_name="tdmpc2" \
         env_id="PlanarReach-v1" num_envs=32 control_mode="pd_joint_delta_pos" env_type=gpu obs=state \
