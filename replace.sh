5191c4a8aab48a8528176aefb403aee2f0aea3b5
Arnav G. <73305591+arnavg115@users.noreply.github.com>
2025-05-30 01:55:32 -0700

    Update replace.sh

--- replace.sh
+++ replace.sh
@@ -19,5 +19,5 @@ pip install -e .
 cd tdmpc2
 
 python train.py model_size=5 steps=1_000_000 buffer_size=500_000 exp_name="tdmpc2" \
-        env_id="PlanarReach-v1" num_envs=32 control_mode="pd_joint_delta_pos" env_type=gpu obs=state \
+        env_id="PlanarReach-v1" num_envs=256 control_mode="pd_joint_delta_pos" env_type=gpu obs=state \
         wandb=$use_wandb wandb_name=tdmpc2-PlanarReach-v1-state-joint-$new_value
